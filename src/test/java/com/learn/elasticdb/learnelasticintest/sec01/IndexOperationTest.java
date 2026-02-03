295dcfe691d6635bdf7206e0dff21a1c4b83b537
meAltf <alataf.dce.cs1@gmail.com>
2026-02-03 23:59:51 +0530

    Improvement | simplify test cases

--- src/test/java/com/learn/elasticdb/learnelasticintest/sec01/IndexOperationTest.java
+++ src/test/java/com/learn/elasticdb/learnelasticintest/sec01/IndexOperationTest.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 
 public class IndexOperationTest extends AbstractTest {
@@ -18,9 +19,21 @@ public class IndexOperationTest extends AbstractTest {
 
     @Test
     public void createIndex(){
-        var indexOperations = this.elasticsearchOperations.indexOps(IndexCoordinates.of("test1"));
+        var indexOperations = this.elasticsearchOperations.indexOps(IndexCoordinates.of("test"));
 
         Assertions.assertTrue(indexOperations.create());
+        this.verify(indexOperations, 1,1);
+    }
+
+    private void verify(IndexOperations indexOperations, int expectedShards, int expectedReplicas){
+        var settings = indexOperations.getSettings();
         log.info("settings: {}", indexOperations.getSettings());
+        log.info("mappings: {}", indexOperations.getMapping());
+
+        Assertions.assertEquals(String.valueOf(expectedShards), settings.get("index.number_of_shards"));
+        Assertions.assertEquals(String.valueOf(expectedReplicas), settings.get("index.number_of_replicas"));
+
+        //delete the index
+        Assertions.assertTrue(indexOperations.delete());
     }
 }
